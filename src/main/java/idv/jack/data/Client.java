fe727da5a038d4ee3d046c1a9c4cce0fac4bfd30
jack <jack@is-land.com.tw>
2018-03-01 12:07:51 +0800

    Modify GenerateData.java for create data

--- src/main/java/idv/jack/data/Client.java
+++ src/main/java/idv/jack/data/Client.java
@@ -2,32 +2,48 @@ package idv.jack.data;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.Random;
 
 public class Client {
     public static void main(String args[]) throws Exception {
-        String resultFilePath = "/home/user1/tool";
-        int fileSize = 9;
+        Random random = new Random();
 
-        for(int fileIndex = 1 ; fileIndex <= fileSize ;fileIndex++) {
+        String resultFilePath = "/home/user1/tool";
+
+        int fileSize = 9;
+        int moduleSize = 3;
+        int hourSize = 24;
+        int daySize = 31;
+
+        for(int fileIndex = 1; fileIndex <= fileSize; fileIndex++) {
             GenerateData generateData = new GenerateData();
             String toolName = generateData.getToolName(1);
-            String moduleName = generateData.getModuleName(1);
-            String date = generateData.getDate("2016", 1, 1, "0" + fileIndex + ":00:00.000");
 
-            String fileName = date + "_" + toolName + "_" + moduleName;
-            BufferedWriter fw = new BufferedWriter(new FileWriter(resultFilePath + "/" + fileName));
+            for(int moduleIndex = 1; moduleIndex <= moduleSize; moduleIndex++) {
+                String moduleName = generateData.getModuleName(moduleIndex);
 
-            int columnNumber = 3;
-            int lines = 20;
+                for(int hourIndex = 0; hourIndex < hourSize; hourIndex++) {
 
-            String header = generateData.getHeader("column", columnNumber);
-            fw.write(header + "\n");
+                    for(int dayIndex = 0; dayIndex < daySize; dayIndex++) {
+                        String date = generateData.getDate("2016", 1, dayIndex, String.format("%02d", hourIndex) + ":00:00.000");
 
-            for (int k = 1; k <= lines; k++) {
-                String value = generateData.getLines(fileIndex, k, columnNumber);
-                fw.write(value + "\n");
+                        String fileName = date + "_" + toolName + "_" + moduleName;
+                        BufferedWriter fw = new BufferedWriter(new FileWriter(resultFilePath + "/" + fileName));
+
+                        int columnSize = random.nextInt(3) + 1;
+                        int lines = random.nextInt(500) + 100;
+
+                        String header = generateData.getHeader("column", columnSize);
+                        fw.write(header + "\n");
+
+                        for (int k = 1; k <= lines; k++) {
+                            String value = generateData.getLines(fileIndex, k, columnSize);
+                            fw.write(value + "\n");
+                        }
+                        fw.close();
+                    }
+                }
             }
-            fw.close();
         }
     }
 }
